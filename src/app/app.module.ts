a087b5d32df10a529a2cb00db113f53a3c274a2e
sijanghm <sijan.ghimire29@gmail.com>
2022-08-03 08:08:10 +0545

    component added for tab based routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NotFoundComponent,
+
   ],
   imports: [
     BrowserModule,
--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -5,12 +5,18 @@ import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionHomeComponent } from './collection-home/collection-home.component';
 import { TableComponent } from './table/table.component';
 import { SharedModule } from '../shared/shared.module';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
 
 @NgModule({
   declarations: [
     CollectionHomeComponent,
-    TableComponent
+    TableComponent,
+    BiographyComponent,
+    CompaniesComponent,
+    PartnersComponent
   ],
   imports: [
     CommonModule,
