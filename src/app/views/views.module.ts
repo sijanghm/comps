21c49526e3901de67b4d171612fdb1ea718cb201
sijanghm <sijan.ghimire29@gmail.com>
2022-08-08 07:16:14 +0545

    view module added

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ViewsRoutingModule } from './views-routing.module';
+import { ViewHomeComponent } from './view-home/view-home.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ViewHomeComponent,
+    StatisticsComponent
+  ],
   imports: [
     CommonModule,
     ViewsRoutingModule
